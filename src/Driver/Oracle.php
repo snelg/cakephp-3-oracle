0691e4183d8b5086c5ded95b290f127c9d5af4e5
Glen Sawyer <glensawyer@hotmail.com>
2015-07-17 14:57:02 -0600

    Do not require "persistent" config variable (defaults to false)

--- src/Driver/Oracle.php
+++ src/Driver/Oracle.php
@@ -64,7 +64,7 @@ class Oracle extends Driver
         $config['init'][] = "ALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD HH24:MI:SS'";
 
         $config['flags'] += [
-            PDO::ATTR_PERSISTENT => $config['persistent'],
+            PDO::ATTR_PERSISTENT => empty($config['persistent']) ? false : $config['persistent'],
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ];
 
