284ae7b89711be3df9b238ed947eac2d7d3db019
Glen Sawyer <glensawyer@hotmail.com>
2016-01-21 12:13:59 -0700

    Fix whitespace inconsistency

--- src/Driver/Oracle.php
+++ src/Driver/Oracle.php
@@ -105,9 +105,9 @@ class Oracle extends Driver
         $statement = new OracleStatement(new PDOStatement($oci8Statement, $this), $this); //And now wrap in a Cake-ified, bufferable Statement
         $statement->queryString = $queryStringRaw; //Oci8PDO does not correctly set read-only $queryString property, so we have a manual override
         if ($isObject) {
-			if ($query->bufferResults() === false || $query->type() != 'select') {
-	            $statement->bufferResults(false);
-			}
+            if ($query->bufferResults() === false || $query->type() != 'select') {
+                $statement->bufferResults(false);
+            }
         }
         return $statement;
     }
